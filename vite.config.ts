21e07f4198eff89ac0aa91dd76876f8d6a354dcf
koby32px <fuelpixelretro@proton.me>
2024-12-17 16:44:44 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/retro-fuel-web/',
+  base: '/',
   build: {
     assetsInlineLimit: 0,
     rollupOptions: {
